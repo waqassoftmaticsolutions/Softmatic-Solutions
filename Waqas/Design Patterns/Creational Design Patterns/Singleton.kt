019883a9d7f2166e75d225d7f270332b88124a16
waqassoftmaticsolutions <muhammad.waqas@softmatic.io>
2024-07-25 12:10:46 +0500

    Update Singleton.kt
    This is singleton Design Pattern

--- Waqas/Design Patterns/Creational Design Patterns/Singleton.kt
+++ Waqas/Design Patterns/Creational Design Patterns/Singleton.kt
@@ -1,2 +1,37 @@
-//The singleton pattern ensures that only one object of a particular class is ever created. All further references to objects of the singleton 
-//class refer to the same underlying instance. There are very few applications, do not overuse this pattern.
+//Creational Design Patterns --> Deals with object creation mechanism
+//1) Singleton 
+//2) Builder
+//3) Prototype
+//4) Factory
+//5) Abstract Factory
+
+//Singleton --> It ensures that a class has only one instance accesing a global access to that instance
+//Compamnion object --> a way to define properties and methods that are associated with the class itself rather than with instances of the class.
+
+class Singleton {
+  private constructor(){}
+  init {
+    println("You are creating an instance of the object")
+  }
+  companion object{
+    private var singleton: Singleton? = null
+    fun getInstance(): Singleton{
+      if(singleton == null){
+        println("Instance has been created successfully")
+        singleton = Singleton()
+      }else{
+        println("Instance already exists")
+      }
+      return singleton!!
+    }
+  }
+}
+
+fun main(){
+  //Singleton Design Pattern
+  var singleton1 = Singleton.getInstance()
+  var singleton2 = Singleton.getInstance()
+  if(singleton1 === singleton2){
+    println("Both are same")
+  }
+}
