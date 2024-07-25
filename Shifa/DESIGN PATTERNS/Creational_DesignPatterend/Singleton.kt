9c98c408aa921fabaee260ff0562350e05b492a4
shifafatimaa <shifa.fatima@softmatic.io>
2024-07-24 23:57:38 -0700

    Update Singleton.kt

--- Shifa/DESIGN PATTERNS/Creational_DesignPatterend/Singleton.kt
+++ Shifa/DESIGN PATTERNS/Creational_DesignPatterend/Singleton.kt
@@ -1,27 +1,24 @@
 //The Singleton Design Pattern ensures that a class has only one instance and provides a global point of access to that instance. 
 //It is commonly used for managing global states, shared resources, or configurations where only one instance is needed throughout the application's lifecycle.
 
-class Singleton private constructor() {
-  companion object {
-      // Single instance of Singleton
-      private var instance: Singleton? = null
+class Singleton private constructor(private val data:String){
+    companion object{
+        
+        private var instance :Singleton?=null;
 
-      // Method to access the instance
-      fun getInstance(): Singleton {
-          if (instance == null) {
-              instance = Singleton()
-          }
-          return instance!!
-      }
-  }
-
-  fun doSomething() {
-      println("Singleton instance doing something.")
-  }
+        fun getInstance(data:String) :Singleton{
+            if(instance==null){
+                instance=Singleton(data)
+            }
+            return instance!!
+        }
+    }
+    fun doSomething(){
+        println("Instance is  $data")
+    }
+    
 }
-
-fun main() {
-  
-  val singletonInstance = Singleton.getInstance()
-  singletonInstance.doSomething() // Output: Singleton instance doing something.
+fun main(){
+    var instance=Singleton.getInstance("Created successfully")
+    instance.doSomething()
 }
